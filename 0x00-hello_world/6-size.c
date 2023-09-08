e9ee13f6c7c8a1d7d8b010efbf4f58415b8de161
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-07 17:17:03 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: a new function
+ * Return: alwys return success
+*/
+int main(void)
+{
+	printf("Size of a char:", sizeof(char));
+	printf("Size of a int:", sizeof(int));
+	printf("Size of a long int:", sizeof(long int));
+	printf("Size of a long long int:", sizeof(long long int));
+	printf("Size of a float:", sizeof(float));
+	return (0);
+}
+
