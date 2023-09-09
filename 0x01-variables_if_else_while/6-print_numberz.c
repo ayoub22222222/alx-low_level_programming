17143413b9ae6e4d16c400f6b944950f61781535
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-08 20:18:02 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Desription: this code generate a randome number
+ *
+ *Return: always (0) for success
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+	putchar('0' + num);
+	num++;
+	}
+	putchar('\n');
+	return (0);
+}
