2e630bf5b4375ce4751b62d800b9b4afd88e8201
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-10 18:58:38 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Desription: this code generate a randome number
+ *
+ *Return: always (0) for success
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + 48);
+		if (num != 9)
+			putchar(',');
+			putchar(' ');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
