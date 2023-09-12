feb6eb6b58b0291f3d5f0f773420e3996e1b356c
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-12 02:50:48 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Desription: this code generate a randome number
+ *
+ *Return: always (0) for success
+*/
+int main(void)
+{
+	char *name = "putchar";
+
+	printf("%s\n", name);
+	return (0);
+}
