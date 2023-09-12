062fd4761dc20f967eb0b9b9a649855696fd701f
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-12 16:44:09 -0700

    qaws

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,16 @@
  *
 */
 
-void	print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	int ch;
 	int count = 0;
 
-	while (count <= 2)
-	for (ch = 'a'; ch <= 'z'; ch++)
-	_putchar(ch);
+	while (count <= 9)
+	{
+	for (ch = 97; ch <= 122; ch++)
+		_putchar(ch);
 	_putchar('\n');
 	count++;
+	}
 }
