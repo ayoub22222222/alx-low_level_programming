57cf98809c243bc60abe46c59f6d5c56f8454721
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-12 15:40:42 -0700

    1-alphab

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ *Desription: this code generate a randome number
+ *
+ *Return: always (0) for success
+*/
 
-
-
-
-
-
-void print_alphabet(void) 
+void print_alphabet(void)
 {
 	int ch;
-	for (ch=97; ch <= 122; ch++)
+
+	for (ch = 97; ch <= 122; ch++)
 		_putchar(ch);
 	_putchar('\n');
-	
+
 }
 
