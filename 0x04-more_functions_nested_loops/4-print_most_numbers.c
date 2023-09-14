41a869dbcdf180b15128d24fe41026e1e46914c0
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-14 11:14:40 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - print number from zero to nine
+ * Return: print a range of number from 0 to 9 and not include 2 and 4
+*/
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+	if (a != 50 && a != 52)
+		_putchar(a);
+	}
+	_putchar('\n');
+}
