7165699b703886bb68dc2fa8a7afc460ccf9eb97
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-14 10:37:59 -0700

    bha

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
  * print_numbers - print number from zero to nine
- *
  * Return: print a range of number from 0 to 9
 */
-int print_numbers(void)
+void print_numbers(void)
 {
 	int a;
 
 	for (a = 48; a <= 57; a++)
 		_putchar(a);
 	_putchar('\n');
-	return (0);
 }
