a9a13f87e9e9b1af2c2ee97dc23add758730e96f
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-14 10:07:23 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiply two number a and b
+ * @a: input for digit one
+ * @b: input for digit two
+ * Return: multiplacation of two number a and b
+*/
+int mul(int a, int b)
+{
+	int results = a * b;
+
+	return (results);
+}
