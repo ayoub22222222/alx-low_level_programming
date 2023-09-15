16c12f42aa4767c3338824580a5e1d6e8867aebb
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-14 18:03:41 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - print line
+ * @size: n is a number that you will take to print the line
+ * Return: print the number of line giving by n number
+*/
+void print_square(int size)
+{
+	int m = 1;
+
+	if (size <= 0)
+	_putchar('\n');
+
+	do {
+	if (size > 0)
+	_putchar('#');
+	++m;
+	} while (m <= n);
+	_putchar('\n');
+}
