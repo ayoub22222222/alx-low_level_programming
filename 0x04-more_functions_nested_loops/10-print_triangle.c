a12ac65a718838c0a22f1cb4796d9e1a81670385
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-15 19:18:42 -0700

    gh

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,7 +14,7 @@ void print_triangle(int size)
 	{
 		for (a = 1; a <= size; a++)
 		{
-			for (b = 1; base <= size; b++)
+			for (b = 1; b <= size; b++)
 			{
 				if ((a + b) <= size)
 					_putchar(' ');
