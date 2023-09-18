1ae8a185852368cab0eedb2f1b084af593564680
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-18 09:26:42 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/** 
+ * swap_int - take two pointers as an integer 
+ * @a: the first int 
+ * @b: the second int 
+ * Return: swap two value between each others 
+ */
+void swap_int(int *a, int *b)
+{
+
+}
