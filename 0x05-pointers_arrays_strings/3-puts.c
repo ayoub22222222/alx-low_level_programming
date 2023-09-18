ea441c0aae17d55a976cdfb0198d6f1f7a69b1c2
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-18 10:50:58 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - is a function that take pointer as parameter
+ * @str: is the name of the variable
+ * Return: print a string followed by new line
+*/
+void	_puts(char *str)
+{
+	char s[] = str;
+
+	for (int i = 0; i <= strlen(s); i++)
+		_putchar(s[i]);
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_rev - function take a char data type 
+ * @s: is a pointer of a name s 
+ * Return: print revers string 
+*/
+void print_rev(char *s)
+{
+	
+}
