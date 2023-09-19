aa7332f0fe2c3896889bfd08ccc0d808aad18c05
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-18 17:19:01 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - take two parametters
+ * @a: the first variable
+ * @n: the second variable
+ * Return: a sequence of int based on the index
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%i", a[i]);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
+
