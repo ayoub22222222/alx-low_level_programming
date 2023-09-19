f200a3be43076a33c5e6d5ce45558c9e1fac065c
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-18 17:43:10 -0700

    pip

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i != (n - 1))
-			printf("%d, ", a[i]);
+			printf("%i, ", a[i]);
 		else
-			printf("%d", a[i]);
+			printf("%i", a[i]);
 	}
 	printf('\n');
 }
