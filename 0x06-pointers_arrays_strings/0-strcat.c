9ed03309dbb27b20a2c9a2f5017e1d3f4b943267
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-22 23:25:35 +0000

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _stract - function that take two argument
+ * @dest: is array pointer
+ * @scr: its also an array
+ * Return: concantinate two string
+*/
+char *_stract(char *dest, char *scr)
+{
+	int a, a2;
+
+	c = 0;
+
+	while (dest[a])
+		a++;
+	for (a2 = 0; src[a2]; a2++)
+		dest[c++] = src[c2];
+
+	return (dest);
+
+}
