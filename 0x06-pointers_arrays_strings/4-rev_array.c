fcac2f750366b292f70d4d021802787223776914
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-22 15:19:38 +0000

    mpo

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - function that take two argument
+ * @a: is array pointer
+ * @n: is number of the element of an array
+ * Return: revers array of integer
+*/
+void reverse_array(int *a, int n)
+{
+        int i;
+
+        for (i = n - 1; i >= 0; --i)
+        {
+                if (i == 0)
+                        printf("%i ", a[i]);
+                else
+                        printf("%i", a[i]);
+        }
+        putchar('\n');
+}
