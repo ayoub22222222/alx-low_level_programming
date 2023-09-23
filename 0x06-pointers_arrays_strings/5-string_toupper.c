17bdb4f14e54864d311143d39a7f14a8ea0a4847
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-23 00:33:05 +0000

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+/**
+ * string_toupper - function take string as an argument
+ * @str: string input
+ * Return: change all lowwer case of a given string into upper case
+*/
+char *string_toupper(char *str)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		if (str[a] >= 97 && str[a] <= 122)
+		{
+			str[a] = str[a] - 32;
+		}
+	}
+	return (str)
+}
