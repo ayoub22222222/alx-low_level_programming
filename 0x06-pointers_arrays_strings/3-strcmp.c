4b1853e0446405ef3e5c3bb0cbccf4f00bf63656
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-23 00:20:13 +0000

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+nclude "main.h"
+/**
+ * _strcmp - function that take two argument as a pametter
+ * @s1: input string number1
+ * @s2: input string number2
+ * Return: compare two string
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int var = 0;
+
+	while (*s2)
+	{
+		if (*s1 != *s2)
+		{
+			var = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (var);
+}
