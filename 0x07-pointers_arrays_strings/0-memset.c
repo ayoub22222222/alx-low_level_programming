a6508836792d522ebafaf7385bab4fc61bdf842f
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-26 23:59:21 +0000

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - take a tree param as variable
+ * @s: is a pointer that point into a char memory adress
+ * @b: is a variable that take a character
+ * @n: its a variable that take the number of bytes
+ * Return: this function take the char b and replace it into the memory adrees
+*/
+char _memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; i < n; a++)
+	{
+	s[a] = b;
+	}
+	return (s);
+}
