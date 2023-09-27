3256b0244f41a89d8dbc329807d7e2a64208caf7
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-27 01:23:35 +0000

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - function that	take tow param
+ * @haystack: variable name
+ * @needle: variable name
+ * Return: locate the substring
+ */
+char *_strstr(char haystack, char needle)
+{
+	int i;
+	int j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+	for (j = 0; needle[j] != '\0'; j++)
+	{
+	if (haystack[i + j] != needle[j])
+	break;
+	}
+	if (!needle[j])
+	return (&haystack[i]);
+	}
+	return (0);
+}
