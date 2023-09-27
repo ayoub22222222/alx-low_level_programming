a0f8d41f3aea7098e50453d8b4cbd5d6dd13ae4d
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-27 00:56:26 +0000

    3-strsp

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,22 +5,22 @@
  * @accept: is a pointer
  * Return: return the length of a prefix substring.
  */
-unsigned int_strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int j = 0;
-	char a = accept;
+	unsigned int c = 0;
+	char t = accept;
 
 	while (s++)
 	{
 	while (accept++)
 	if ((s - 1) == (accept - 1))
 	{
-	j++;
+	c++;
 	break;
 	}
 	if (!(--accept))
 	break;
-	accept = a;
+	accept = t;
 	}
 	return (c);
 }
