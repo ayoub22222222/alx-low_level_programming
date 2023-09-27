5be2d38e593863ec4e6441c1a88afd354766c1a3
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-27 00:34:23 +0000

    1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * memcpy - this function take tree param as a variable
+ * _memcpy - this function take tree param as a variable
  * @dest: its a pointer that point into an adress memory
  * @src: pointer that point into a memory adress
  * @n: variable n that take a number
@@ -8,13 +8,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        int j = 0;
-        int q = n;
+	int j = 0;
+	int q = n;
 
-        for (; j < q; j++)
-        {
-                dest[j] = src[j];
-                n--;
-        }
-        return (dest);
+	for (; j < q; j++)
+	{
+	dest[j] = src[j];
+	n--;
+	}
+	return (dest);
 }
