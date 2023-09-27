8e0db8dcf8b97b990190b7209f969d8d70a7e762
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-27 01:59:29 +0000

    io

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,8 +17,8 @@ void print_diagsums(int a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-	s1 += a[(size i) + i];
-	s2 += a[(size * (i + 1)) - (i + 1)];
+		s1 += a[(size i) + i];
+		s2 += a[(size * (i + 1)) - (i + 1)];
 	}
 
 	printf("%d, %d\n", s1, s2);
