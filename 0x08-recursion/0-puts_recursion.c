4d821cf27e65a83913bd16d3ecf32ccf32041226
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-29 02:31:10 +0000

    0-puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s <= '\0')
+	if (*s == '\0')
 	{
-		_puts_recursion(s + 1);
-		_putchar(*s);
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
