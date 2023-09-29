3f89d8feb9b1f9dadf1559036d1c65cc67da6d60
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-29 02:08:18 +0000

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that take on arg as a parametter
+ * @s: variable that take a string as a value
+ * Return: return a sequence as a string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s < '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
