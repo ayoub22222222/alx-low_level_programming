3b4dc90fa7f8c4d753e09a18ef7a31cecb42665d
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-09-30 02:35:13 +0000

    pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that raise the power of a number
+ * @x: integer number one
+ * @y: intger number two
+ * Return: return the power of the number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 1)
+		return (x);
+
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,6 @@ int _putchar(char c);
 int factorial(int n);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
 
 #endif
