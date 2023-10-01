4b0c1d45970dcf7ef8c396604d5bd3f08ae60c39
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-10-01 19:54:08 +0000

    6-is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "mai.h"
+#include <stdio.h>
+/**
+ * is_prime_number - check if n is prime number
+ * @othrn: int
+ * Return: return bolean value 0 or 1
+ */
+int check_prime(int n, int othrn);
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+/**
+* check_prime - check all the number < n if they can devide it
+* @n: int
+* @othrn: int
+* Return: int
+*/
+int check_prime(int n, int othrn)
+{
+	if (othrn >= n && n > 1)
+		return (1);
+	else if (n % othrn == 0 || n <= 1)
+		return (0);
+	else
+		return (check_prime(n, othrn + 1));
+}
