9e5a8f2989cdd787b0b926f044c11fa29fe72858
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-10-02 19:46:47 -0700

    j

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	int result;
 
 	if (argc == 3)
-		result = atoi(argv[1]) * atoi(argv[2])
+		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", result);
 	else
 	{
