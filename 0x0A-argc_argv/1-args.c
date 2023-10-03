3ac723fd6c775a41d65b0ea91045ed470ff0c808
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-10-02 18:58:32 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: return the neumber ofargument passed on it
+*/
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%i\n", argv[argc]);
+	return (0);
+}
