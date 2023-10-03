c96975f4ddc28007c2b8f92309514e896d1156b8
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-10-02 18:46:46 -0700

    p

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument couter
+ * @argv: argument vector
+ * Return: return the name of the programe
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
