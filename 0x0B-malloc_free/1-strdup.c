de49b151c60cf0f66bc9081406e4d9f0e73e8f44
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-10-04 18:16:00 -0700

    1-st

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,11 +9,18 @@
 char *_strdup(char *str)
 {
 	int i;
-	int size = sizeof(str) + 1;
-	char *pstr = malloc(sizeof(char) * size);
+	int r;
+
+	r = 0;
+
+	while (str[r] != '\0')
+		r++;
+
+	char *pstr = malloc(sizeof(char) * (r + 1);
 
 	if (pstr == NULL)
 		return (NULL);
+
 	for (i = 0; i <= size; i++)
 		pstr[i] = str[i];
 	return (pstr);
