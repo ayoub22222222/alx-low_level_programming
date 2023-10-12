09011e507a2acd4892110322acd41a49641f45e0
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-10-12 10:40:00 -0700

    l

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,5 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-
-#include <stddef.h>
-#include <stdlib.h>
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
