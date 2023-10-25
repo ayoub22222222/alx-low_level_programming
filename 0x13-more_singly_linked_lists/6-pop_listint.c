85693a08cd7bf1fddaa175903bc349ef8afd2f8f
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-10-24 17:20:14 -0700

    p

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - funtion that deletes
+ * @head: argument function
+ * Return: remove the head of a function
+*/
+int pop_listint(listint_t **head)
+{
+	litsint_t *node;
+	int n;
+
+	if (!head || *head)
+		return (0);
+
+	node = (*head)->next;
+	n = (*head)->n;
+	*head = node;
+	return (n);
+
+}
