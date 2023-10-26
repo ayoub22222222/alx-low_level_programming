4f738564527781d90341f9ed3bb4541c52f1e7d1
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-10-26 14:48:39 -0700

    p

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_bit - function
+ * @n: parametters
+ * @index: paramters
+ * Return: the value of the bit
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int div, res;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	div = 1 << index;
+	res = n & div;
+	if (res == div)
+		return (1);
+
+	return (0);
+}
