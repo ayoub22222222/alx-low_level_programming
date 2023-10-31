4046cd57d54736a1f98af2f2c47ebe028cf521a6
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-10-30 20:03:23 -0700

    p

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,14 +7,14 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, write, len = 0;
+	int fd, wr, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (file == -1)
+	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
@@ -22,12 +22,12 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 
-		write = write(file, text_content, len);
+		wr = write(fd, text_content, len);
 
-		if (write == -1)
+		if (wr == -1)
 			return (-1);
 	}
 
-	close(file);
+	close(fd);
 	return (1);
 }
