47a9b2d5cec2986880f37e85f92bf97cb4962eb0
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-10-30 19:24:44 -0700

    1-create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * create_file - function that create a file
+ * @filename: the name of the file as a parametters
+ * @text_content: character pointer to a text input
+ * Return: create a new file
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int len;
+	int wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_RDWR, 0600);
+
+	if (file < 0)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+		w = write(file, text_content, len);
+		if (wr < 0)
+			return (-1);
+	}
+	close(file);
+	return (1);
+}
