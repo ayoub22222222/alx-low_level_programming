ae9fb3bdd3a3c9ac4aeee9d5749ae31f1b9f1fbc
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-10-30 19:35:12 -0700

    h

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_RDWR, 0600);
+	file = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
 
 	if (file < 0)
 		return (-1);
