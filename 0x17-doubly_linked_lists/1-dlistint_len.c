e7e8f6513962e8ec4b9d0f9d97474572b1aecb24
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-12-07 15:54:42 -0800

    1-dlistint_len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * dlistint_len - a function that calculate the lenght of a linked list
+ * @h: is a poiter to a structure
+ * Return: the lenght of a lined list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	int counter = 0;
+	if (h == NULL)
+		return (0);
+
+	while (h->next)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter)
+}
+	
