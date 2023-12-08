7e581bfdc4b673208fa3371be991b04ba0e403d8
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-12-07 18:38:03 -0800

    4-free_dlistint

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_dlistint - function that free a given node
+ * @head: the pointer to a the first node
+ * Return: free a given node
+*/
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return (NULL);
+	current = head->next;
+
+	while (current != NULL)
+	{
+		free(head);
+		current = current->next;
+	}
+	return (head);
+}
