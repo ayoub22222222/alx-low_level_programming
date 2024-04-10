4da1d17f86cf9c6237d1ca30434a25b43545a832
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-04-09 17:39:32 -0700

    task 0-0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,14 +10,15 @@
 int linear_search(int *array, size_t size, int value)
 {
 	int i;
-	if (array == NULL):
+
+	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
 		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return i;
+		if (value == array[i])
+			return (i);
 	}
 	return (-1);
 }
