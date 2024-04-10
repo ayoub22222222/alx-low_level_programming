b35f4f5565e706546dd190d955fbd799c06969bb
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-04-09 17:37:03 -0700

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+/**
+ * linear_search - function name
+ * @array: arrgument pointer
+ * @size: size of an array
+ * @value: target value
+ * Return: return the index of a given value
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+	if (array == NULL):
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return i;
+	}
+	return (-1);
+}
